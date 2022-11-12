b23add2990dfd4a27507f08a8715a4e5d42535e9
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2022-11-12 12:18:19 +0530

    made feature section responsive

--- components/Feature/Feature.jsx
+++ components/Feature/Feature.jsx
@@ -1,14 +1,18 @@
+import styles from "./Feature.module.scss";
 export default function Feature() {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="flex flex-col justify-center mb-6 lg:flex-row md:mb-10">
-        <div className="lg:w-1/2">
-          <h1 className="max-w-md mb-6 font-sans text-3xl font-bold tracking-tight text-white-900 sm:text-4xl sm:leading-none xl:max-w-lg">
+        <div className="lg:w-1/2 ">
+          <h1
+            className="max-w-md leading-tight  mb-6 min-h-fit font-sans text-center text-3xl font-bold tracking-tight  text-white-900 sm:text-4xl sm:leading-none xl:max-w-lg"
+            style={{ fontSize: "4rem" }}
+          >
             What we offer?
           </h1>
         </div>
         <div className="lg:w-1/2">
-          <p className="text-base text-white-700 md:text-lg">
+          <p className="text-base text-center text-white-700 md:text-lg">
             Through our vision of "Learn, Build and Scale" we implement various
             initiatives and events in IIT BHU to foster the entrepreneurial
             minds and create a culture of enthralling startups bound for
@@ -16,8 +20,8 @@ export default function Feature() {
           </p>
         </div>
       </div>
-      <div className="grid gap-8 row-gap-10 sm:grid-cols-2  lg:grid-cols-4">
-        <div className="max-w-md ">
+      <div className="grid gap-8 row-gap-10 grid-cols-2 sm:grid-cols-2  lg:grid-cols-4 2xl:grid-cols-6">
+        <div className="max-w-md flex items-center justify-center flex-col ">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -33,14 +37,14 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
             Free E-Summit 2023 Passes
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -56,14 +60,17 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             When has justice
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            50% off on GrowthSchool Workshops
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            50% off on GrowthSchool 
+          </p>
+          <p className="mb-3 text-sm text-center text-white-900">
+          Workshops
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -79,14 +86,17 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             Organically grow
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            E-Learn Markets Financial Course Worth 999
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            E-Learn Markets Financial 
+          </p>
+          <p className="mb-3 text-sm text-center text-white-900">
+          Course Worth 999
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -102,14 +112,41 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             A slice of heaven
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            2C Pro Memberships worth 999 (Top 50 Performers)
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            D2C Pro Memberships worth 999 
+          </p>
+          <p className="mb-3 text-sm text-center text-white-900">(Top 50 Performers)</p>
+        </div>
+        <div className="max-w-md flex items-center justify-center flex-col">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+            <svg
+              className="w-12 h-12 text-purple-600"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+            The deep ocean
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            Access to the Entrepreneurship 
+          </p>
+          <p className="mb-3 text-sm text-center text-white-900">
+          Masterclass
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -125,14 +162,38 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            Access to the Entrepreneurship Masterclass
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">GeeksforGeeks Coupons</p>
+        </div>
+        <div className="max-w-md flex items-center justify-center flex-col">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+            <svg
+              className="w-12 h-12 text-purple-600"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+            The deep ocean
+          </h6> */}
+          <p className="mb-3 text-center text-sm text-white-900">
+            Access to Curated Startup Helpbook
+          </p>
+          <p className="mb-3 text-center text-sm text-white-900">
+           of E-Cell IIT BHU
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -148,35 +209,17 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">GeeksforGeeks Coupons</p>
-        </div>
-        <div className="max-w-md">
-          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg
-              className="w-12 h-12 text-purple-600"
-              stroke="currentColor"
-              viewBox="0 0 52 52"
-            >
-              <polygon
-                strokeWidth="3"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                fill="none"
-                points="29 13 14 29 25 29 23 39 38 23 27 23"
-              />
-            </svg>
-          </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
-            The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            Access to Curated Startup Helpbook of E-Cell IIT BHU
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            Top CDs to be featured in the E-Cell IIT BHU’s 
+          </p>
+          <p className="mb-3 text-sm text-center text-white-900">
+          Weekly Newsletter
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -192,14 +235,17 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            op CDs to be featured in the E-Cell IIT BHU’s Weekly Newsletter
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            Certificate of CD Programme verified by 
+          </p>
+          <p className="mb-3 text-sm text-center text-white-900">
+          E-Cell IIT BHU
           </p>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md flex items-center justify-center flex-col">
           <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
             <svg
               className="w-12 h-12 text-purple-600"
@@ -215,34 +261,14 @@ export default function Feature() {
               />
             </svg>
           </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
+          {/* <h6 className="mb-2 font-semibold leading-5 text-purple-600">
             The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            Certificate of CD Programme verified by E-Cell IIT BHU
+          </h6> */}
+          <p className="mb-3 text-sm text-center text-white-900">
+            Free E-Summit 2022 Merch to the 
           </p>
-        </div>
-        <div className="max-w-md">
-          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
-            <svg
-              className="w-12 h-12 text-purple-600"
-              stroke="currentColor"
-              viewBox="0 0 52 52"
-            >
-              <polygon
-                strokeWidth="3"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                fill="none"
-                points="29 13 14 29 25 29 23 39 38 23 27 23"
-              />
-            </svg>
-          </div>
-          <h6 className="mb-2 font-semibold leading-5 text-purple-600">
-            The deep ocean
-          </h6>
-          <p className="mb-3 text-sm text-white-900">
-            Free E-Summit 2022 Merch to the Top 5 Performers
+          <p className="mb-3 text-sm text-center text-white-900">
+          (Top 5 Performers)
           </p>
         </div>
       </div>
