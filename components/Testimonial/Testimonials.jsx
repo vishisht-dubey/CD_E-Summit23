f44242b75ccdf9c74111f87f474a1e1bea700593
Prakhar Agarwal <prakharagarwal3031@gmail.com>
2022-11-15 15:28:25 +0530

    fixed attribute errors

--- components/Testimonial/Testimonials.jsx
+++ components/Testimonial/Testimonials.jsx
@@ -27,7 +27,7 @@ const Testimonials = () => {
               >
                 <path
                   strokeLinecap="round"
-                  stroke-linejoin="round"
+                  strokeLinejoin="round"
                   d="M15 19l-7-7 7-7"
                 />
               </svg>
@@ -68,11 +68,11 @@ const Testimonials = () => {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
                   strokeLinecap="round"
-                  stroke-linejoin="round"
+                  strokeLinejoin="round"
                   d="M9 5l7 7-7 7"
                 />
               </svg>
