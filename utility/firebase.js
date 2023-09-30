9d13ad16138dbab89ed7673b5378132f65b41bed
vishisht-dubey <vishisht.dubey.phe21@itbhu.ac.in>
2023-09-30 08:46:59 +0530

    using e-cell summit-23 firebase

--- utility/firebase.js
+++ utility/firebase.js
@@ -7,24 +7,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCUstIzj4rkWDDwj54YDeYPKirixR8yFjQ",
-//   authDomain: "e-summit-23.firebaseapp.com",
-//   projectId: "e-summit-23",
-//   storageBucket: "e-summit-23.appspot.com",
-//   messagingSenderId: "993378576778",
-//   appId: "1:993378576778:web:719dec6168ae456936c073",
-//   measurementId: "G-1RP58LL44S",
-//   databaseURL: "https://e-summit-23-default-rtdb.firebaseio.com",
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyD0p1bNaCfVeP5M2RPiiZVqKarjEZwm9CA",
-  authDomain: "portfolio-d2956.firebaseapp.com",
-  projectId: "portfolio-d2956",
-  storageBucket: "portfolio-d2956.appspot.com",
-  messagingSenderId: "819926489468",
-  appId: "1:819926489468:web:355ab8114761a2d51bacf9",
-  measurementId: "G-QKS84BFJKL",
+  apiKey: "AIzaSyCUstIzj4rkWDDwj54YDeYPKirixR8yFjQ",
+  authDomain: "e-summit-23.firebaseapp.com",
+  databaseURL: "https://e-summit-23-default-rtdb.firebaseio.com",
+  projectId: "e-summit-23",
+  storageBucket: "e-summit-23.appspot.com",
+  messagingSenderId: "993378576778",
+  appId: "1:993378576778:web:719dec6168ae456936c073",
+  measurementId: "G-1RP58LL44S",
 };
 
 // Initialize Firebase
